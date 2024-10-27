b0d053a3fb1fc2ed8607f902958ccd67948f0e2e
xLevitate <levitatewebhost@hotmail.com>
2024-10-27 10:18:21 -0300

    updated to work with Quill 2.1.0

--- src/main/java/me/levitate/plugintemplate/PluginTemplate.java
+++ src/main/java/me/levitate/plugintemplate/PluginTemplate.java
@@ -1,17 +1,14 @@
 package me.levitate.plugintemplate;
 
 import lombok.Getter;
-import me.levitate.quill.api.QuillAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public final class PluginTemplate extends JavaPlugin {
-    private QuillAPI quill;
 
     @Override
     public void onEnable() {
-        quill = QuillAPI.builder()
-                .plugin(this)
-                .build();
+        // Plugin startup
     }
+
 }
